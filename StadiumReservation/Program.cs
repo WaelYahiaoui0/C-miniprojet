796f7c2227f51946445f3975ec5f2b23593e4fe8
WaelYahiaoui0 <117662871+WaelYahiaoui0@users.noreply.github.com>
2022-11-15 09:56:53 +0100

    init

---
+++ StadiumReservation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
